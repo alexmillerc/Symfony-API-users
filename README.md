9bd9f7c4cfad58989a011b81e9aee5b194a287af
Alex Miller <alexmillerc@gmail.com>
2021-04-25 13:04:30 -0300

    Includes new Symfony API Users Project

---
+++ README.md
@@ -0,0 +1,5 @@
+# PHP - Symfony API user - Alex Miller
+
+## Project setup
+
+php composer install
---
+++ src/Entity/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="firstName is required")
+     * @Assert\Length(
+     *     min=3,
+     *     max=100,
+     *     minMessage="firstName needs to have at least 3 characters"
+     * )
+     */
+    private string $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="lastName is required")
+     * @Assert\Length(
+     *     min=3,
+     *     max=100,
+     *     minMessage="lastName needs to have at least 3 characters"
+     * )
+     */
+    private string $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="email is required")
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email."
+     * )
+     */
+    private string $email;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private \DateTime $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?\DateTime $updatedAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->updatedAt = new \DateTime();
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->updatedAt = new \DateTime();
+        $this->lastName = $lastName;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->updatedAt = new \DateTime();
+        $this->email = $email;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+}
